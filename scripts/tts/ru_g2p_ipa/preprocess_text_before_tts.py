a0b37f3a8631c182bec238b0f15bbc943f8a7ebe
Alexandra Antonova <antonova_sasha@list.ru>
2023-05-03 19:49:55 +0300

    add g2p scripts
    Signed-off-by: Alexandra Antonova <antonova_sasha@list.ru>

---
+++ scripts/tts/ru_g2p_ipa/preprocess_text_before_tts.py
@@ -0,0 +1,79 @@
+import re
+
+from argparse import ArgumentParser
+
+from utils import (
+    clean_russian_g2p_trascription,
+    clean_russian_text_for_tts,
+)
+
+
+parser = ArgumentParser(description="Prepare input for Russian tts inference")
+parser.add_argument("--input_name", type=str, required=True, help="Input file with plain text")
+parser.add_argument("--g2p_name", type=str, required=True, help="Results of g2p, each unique word on a separate line")
+parser.add_argument("--heteronyms_name", type=str, required=True, help="Text file with words that can have different transcriptions, they will be kept as graphemes")
+parser.add_argument("--g2p_correct_name", type=str, required=True, help="Text file with words and transcriptions that override g2p output")
+parser.add_argument("--output_name", type=str, required=True, help="Output file with input to tts")
+
+args = parser.parse_args()
+
+heteronyms = set()
+with open(args.heteronyms_name, "r", encoding="utf-8") as f:
+    for line in f:
+        inp = line.strip()
+        heteronyms.add(inp)
+
+
+g2p_vocab = {}
+
+with open(args.g2p_name, "r", encoding="utf-8") as f:
+    for line in f:
+        try:
+            # Example input: b lʲ 'ʉ xʲ ɪ r  б л ю х е р     b lʲ 'ʉ xʲ ɪ r  b lʲ 'ʉ xʲ ɪ r  PLAIN PLAIN PLAIN PLAIN PLAIN PLAIN
+            _, inp, transcription, _, _ = line.strip().split("\t")
+        except:
+            print("cannot read line: " + line)
+            continue
+        inp = inp.replace(" ", "")
+        g2p_vocab[inp] = clean_russian_g2p_trascription(transcription)
+
+with open(args.g2p_correct_name, "r", encoding="utf-8") as f:
+    for line in f:
+        try:
+            # Example input: ледок   lʲɪd`ok
+            inp, transcription = line.strip().split("\t")
+        except:
+            print("cannot read line: " + line)
+            continue
+        g2p_vocab[inp] = transcription
+
+out = open(args.output_name, "w", encoding="utf-8")
+
+with open(args.input_name, "r", encoding="utf-8") as inp:
+    for line in inp:
+        text = line.strip()
+        text = clean_russian_text_for_tts(text)
+        phonemized_text = ""
+        m = re.search(r"[\w\-]+", text)
+        while m is not None:
+            begin = m.start()
+            end = m.end()
+            phonemized_text += text[0:begin]
+            w = text[begin:end]
+            if w in heteronyms:
+                phonemized_text += w
+            elif w in g2p_vocab:
+                phonemized_text += clean_russian_g2p_trascription(g2p_vocab[w])
+            else:  # shouldn't go here as all words are expected to pass through g2p
+                phonemized_text += w
+
+            if end >= len(text):
+                break
+            text = text[end:]
+            m = re.search(r"[\w\-]+", text)
+        if end < len(text):
+            phonemized_text += text[end:]
+        
+        out.write(phonemized_text + "\n")
+
+out.close()
---
+++ scripts/tts/ru_g2p_ipa/utils.py
@@ -0,0 +1,30 @@
+import re
+
+"""Utility functions for Russian grapheme-to-phoneme conversion using IPA-like alphabet"""
+
+def clean_russian_g2p_trascription(text: str) -> str:
+    result = text
+    result = result.replace("<DELETE>", " ").replace("+", "").replace("~", "")
+    result = result.replace("ʑ", "ɕ:").replace("ɣ", "x")
+    result = result.replace(":", "ː").replace("'", "`")
+    result = "".join(result.split())
+    result = result.replace("_", " ")
+    return result
+
+
+def clean_russian_text_for_tts(text: str) -> str:
+    result = text
+    result = result.replace("+", "")  # remove stress
+    result = result.casefold()  # lowercase
+    result = result.replace("ё", "е")
+    result = result.replace("\u2011", "-")  # non-breaking hyphen
+    result = result.replace("\u2014", "-")  # em dash
+    result = result.replace("\u2026", ".")  # horizontal ellipsis
+    result = result.replace("\u00ab", "\"")  # LEFT-POINTING DOUBLE ANGLE QUOTATION MARK
+    result = result.replace("\u00bb", "\"")  # RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK
+    result = result.replace("\u201c", "\"")  # “ Left Double Quotation Mark
+    result = result.replace("\u201d", "\"")  # ” Right Double Quotation Mark
+    result = result.replace("\u201e", "\"")  # „ Double Low-9 Quotation Mark
+    result = result.replace("\u201f", "\"")  # ‟ Double High-reversed-9 Quotation Mark
+    return result
+
